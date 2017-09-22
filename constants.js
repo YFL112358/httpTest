7ab0392467abfdf5fce70e6829bccec0f783c155
shareming7 <2395277764@qq.com>
2017-09-22 11:55:35 +0800

    updated constants.js

--- constants.js
+++ constants.js
@@ -1,10 +1,17 @@
 'use-strict';
 exports.VERSON = {
+    VERSION_3: '/v3'    
     
+}
+exports.PATHS = {
+    INDEX: "/index",
+    API_TEST: "/api_test",    
+}
+exports.PARAMS = {
+    ACTION: "/:action",
 }
 exports.RET_CODE = {
   OK: 1000,
-  FAILURE: 1001,
 }
 
 
--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const constants = require('./constants')
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const v3Router = express.Router();
 
 app.use(bodyParser.json()); // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-const v3Router = express.Router();
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded  
+
 //3.1
-v3Router.get('/test', function (req, res) {
+v3Router.get(constants.PATHS.API_TEST, function (req, res) {
     res.send('hello');   
 });
 
 //3.2
-v3Router.get('/:action', function (req, res) {
+v3Router.get(constants.PARAMS.ACTION, function (req, res) {
     const sum = parseInt(req.query.a) + parseInt(req.query.b);
     res.send({ret: constants.RET_CODE.OK, action: req.params.action, result: sum});
 });
@@ -30,7 +31,7 @@ v3Router.post('/:action' ,function(req, res){
         'ret'    : constants.RET_CODE.OK,
     });    
 });
-app.use('/v3', v3Router);
+app.use(constants.VERSON.VERSION_3, v3Router);
 
 //3.4
 
@@ -38,7 +39,7 @@ app.use('/v3', v3Router);
 //app.set('view engine','pug');
 //app.set('views', './views');
 
-app.use('/index', express.static('public'))
+app.use(constants.PATHS.INDEX, express.static('public'))
 
 var server = app.listen(3000, function () {
 var host = server.address().address;
