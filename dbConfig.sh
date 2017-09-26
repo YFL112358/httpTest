a883d9d20d606e5a388d8abbafc99fefef7b56c6
shareming7 <2395277764@qq.com>
2017-09-26 21:46:04 +0800

    updated dbConfig.sh

--- dbConfig.sh
+++ dbConfig.sh
@@ -1,12 +1,20 @@
 basedir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 dbuser=root
-dbpwd=123456
+dbpwd=
 dbname=httpTest
 
 cp $basedir/config/mysql.conf.template $basedir/config/mysql.conf
 
-mysql -uroot -p$dbpwd -e CREATE DATABASE $dbname
 
-mysql -uroot -p$dbpwd $dbname < $basedir/httpTest.sql  
+if [ ! -z $dbpwd ]; then
+  mysql -uroot -p"\'$dbpwd\'" -e "CREATE DATABASE $dbname"
+else
+  mysql -uroot -e "CREATE DATABASE $dbname"
+fi
 
+if [ ! -z $dbpwd ]; then
+  mysql -uroot -p"\'$dbpwd\'" $dbname < $basedir/httpTest.sql  
+else
+  mysql -uroot $dbname < $basedir/httpTest.sql  
+fi
 
